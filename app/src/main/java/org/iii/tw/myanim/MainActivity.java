0708c2fe0858ab5f35ec4569bd855b276dd4f83f
chuan1983 <chuan1983126@gmail.com>
2016-09-22 11:01:44 +0800

    v1

---
+++ app/src/main/java/org/iii/tw/myanim/MainActivity.java
@@ -0,0 +1,23 @@
+package org.iii.tw.myanim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.img);
+
+    }
+
+    public void test1(View v){
+
+    }
+}
